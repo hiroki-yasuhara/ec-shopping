527f633973d82548e0c48b1170ce51a7f8d69a07
hiroki.yasuhara <hiroki.yasuhara29@gmail.com>
2020-03-24 10:48:54 +0000

    version1

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+    validates :name,  presence: true, length: { maximum:50 }
+    validates :email, presence: true, length: { maximum: 255 },
+                      format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                      uniqueness: { case_sensitive: false }
+    validates :bank,  presence: true, length: { maximum:20 }
+    
+    has_secure_password
+end
