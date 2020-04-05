2213c60a4944a802f46078efbf90cc30ec62b59d
hiroki.yasuhara <hiroki.yasuhara29@gmail.com>
2020-04-05 07:54:06 +0000

    for heroku1-1

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ unless Rails.env.development? || Rails.env.test?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: 'AKIA5KM6ZOJJFCFEFCFT',
-      aws_secret_access_key: '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',
+      aws_access_key_id: '',
+      aws_secret_access_key: '',
       region: 'ap-northeast-1',
     }
      config.fog_directory  = 'rails-image-hiroki'
