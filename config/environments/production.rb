0bf07fc39c73807206d4ed7a8ba50e0540568a3c
hiroki.yasuhara <hiroki.yasuhara29@gmail.com>
2020-04-05 12:27:41 +0000

    for heroku1-6

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port:                 587,
+    address:              'smtp.gmail.com',
+    domain:               'gmail.com',
+    user_name:            'hiroki.webkanri99',
+    password:             '48694389',
+    authentication:       'login',
+    enable_starttls_auto: true
+  }
 end
